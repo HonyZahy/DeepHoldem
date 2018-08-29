7d42be674fcb7e216f7b0d1f4096b30f4cf66892
HonyZahy <42115839+HonyZahy@users.noreply.github.com>
2018-08-29 16:44:02 +0200

    Update evaluator.lua

--- Source/Game/Evaluation/evaluator.lua
+++ Source/Game/Evaluation/evaluator.lua
@@ -28,17 +28,21 @@ function M:_init()
     end
   end
   if self._texas_lookup == nil then
-    local f = assert(io.open("./Game/Evaluation/HandRanks.dat", "rb"))
-    local data = f:read("*all")
-    self._texas_lookup = arguments.Tensor(string.len(data) / 4):fill(0):long()
-    for i = 1, string.len(data), 4 do
-      local num = 0
-      for j = i,i+3 do
-        num = num + data:byte(j) * (2 ^ ((j - i) * 8))
+    if os.isfile("./Game/Evaluation/HandRanksTensor.dat") then
+      self._texas_lookup = torch.load("./Game/Evaluation/HandRanksTensor.dat")
+    else
+      local f = assert(io.open("./Game/Evaluation/HandRanks.dat", "rb"))
+      local data = f:read("*all")
+      self._texas_lookup = arguments.Tensor(string.len(data) / 4):fill(0):long()
+      for i = 1, string.len(data), 4 do
+        local num = 0
+        for j = i,i+3 do
+          num = num + data:byte(j) * (2 ^ ((j - i) * 8))
+        end
+        self._texas_lookup[(i - 1) / 4 + 1] = num
       end
-      self._texas_lookup[(i - 1) / 4 + 1] = num
+      f:close()
     end
-    f:close()
     if arguments.gpu then
       self._texas_lookup = self._texas_lookup:cudaLong()
     end
